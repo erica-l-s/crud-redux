5e35a7b7670785aed4480af6753acf08f0c1c960
erica-l-s <erica.l.s1996@gmail.com>
2023-04-18 22:33:07 -0300

    get name user logged in

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import {useDispatch} from "react-redux"
-import { logout } from '../feature/userSlice';
+import {useDispatch,useSelector} from "react-redux"
+import { logout, selectUser } from '../feature/userSlice';
 
 const Create = () => {
-    
+    const user = useSelector(selectUser);
+
 const dispatch = useDispatch()
 const handleLogout = (e) =>{
    e.preventDefault();
@@ -13,7 +14,7 @@ const handleLogout = (e) =>{
 
   return (
     <div>
-        <h1>Welcome</h1>
+        <h1>Welcome <span>{user.name}</span></h1>
         <button onClick={(e)=> handleLogout(e)}>Logout</button>
     </div>
   )
